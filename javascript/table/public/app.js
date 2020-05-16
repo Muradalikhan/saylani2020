ea33f40498e57b9801080bd800a9711804f5c4b8
Murad khan <murad.jaddoon@gmail.com>
2020-05-17 03:08:21 +0500

    printing user's desrie table

---
+++ javascript/table/public/app.js
@@ -0,0 +1,46 @@
+
+// ************odd number *************
+
+// for(i=0; i<10;i++)
+// {
+//     console.log(++i);
+// }
+
+
+// ************even number *************
+
+// for(i=0; i<10; i++){
+
+//     console.log(i++);
+// }
+
+
+// ************table of user wish *************
+
+// var result=document.getElementById("print-table");
+// var a=prompt("which table you want to print ")
+// for(i=1; i<11; i++){
+   
+// result.innerHTML +=a +" x "+ i +" = "+ i*a +"<br/>"
+
+// }
+
+
+
+
+// ************Sign in *************
+
+// var email=prompt("what is your email");
+
+// var password=prompt("what is your password");
+
+// if(email==="murad" && password==="1234"){
+//     alert("murad has logged in")
+// }
+// else if(email==="khan" && password==="123"){
+//     alert("khan has logged in")
+// }
+// else{
+//     alert("no user find")
+// }
+
