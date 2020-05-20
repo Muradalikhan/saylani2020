1f3ff41c08e040dec5923ed26684b8d1d3e3bac5
Murad khan <murad.jaddoon@gmail.com>
2020-05-20 15:54:39 +0500

    switch case

---
+++ javascript/switch case/public/app.js
@@ -0,0 +1,36 @@
+
+
+// var userInput=prompt("please enter current day name");
+// var userInput2=prompt("What do you want these two numbers 10,5  add,subtract,multiply,divide");
+
+// switch(userInput2){
+//     case 'add':
+//     alert(10+5);
+//     break;
+//     case 'subtract':
+//     alert(10-1);
+//     break;
+//     case 'multiply':
+//     alert(10*5);
+//     break;
+//     case 'divide':
+//     alert(10/5);
+//     break;
+//     default:
+//     alert("you entered wrong name");
+
+// }
+
+
+
+
+var i=0;
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+
+
+
+
