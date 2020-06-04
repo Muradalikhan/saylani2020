9b36f75a1f8dbdd87eff36a709edbe89b017f1d1
Murad khan <murad.jaddoon@gmail.com>
2020-06-04 10:35:07 +0500

    random and rounding nubmer

---
+++ javascript/random number and round/public/app.js
@@ -0,0 +1,30 @@
+
+// generating random number from 1 - 6
+
+// var num=Math.random();
+// num=num*6+1;
+// num=Math.floor(num);
+// console.log(num);
+
+
+
+
+// converting string to integer 
+
+// var userAge=prompt("Enter your Age please");
+// // var sumUp=userAge+1;  // here it will concate the number due to string value
+// var sumUp=parseInt(userAge)+1;
+// console.log(sumUp);
+
+
+
+// converting string to number 
+
+// var userAge=prompt("Enter your Age please");
+// var sumUp=parseInt(userAge)+1;
+//     sumUp=sumUp.toString();
+//     sumUp=sumUp+1
+// console.log(sumUp);
+
+
+
