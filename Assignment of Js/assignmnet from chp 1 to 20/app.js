5424742d2cceb21af5cef7e49a2439019d1f38f0
Murad khan <murad.jaddoon@gmail.com>
2020-06-05 21:20:21 +0500

    JS Assignment

---
+++ Assignment of Js/assignmnet from chp 1 to 20/app.js
@@ -0,0 +1,108 @@
+
+// Assignment #1
+//----Alert-------
+
+// alert('Welcome to our website');
+
+// alert('Error! please enter a valid password');
+
+// alert(`welcome to JS Land...\nHappy Coding!`);
+
+
+
+
+// Assignment #2
+//----VARIABLES FOR STRINGS -------
+
+// //1
+// var username;
+
+// //2
+// var myName='Murad Ali'
+
+// //3
+// var message;
+//     message='Hello World';
+//     alert(message);
+
+//4
+// var stdName='Jhone Doe';
+// var stdAge='15 years old';
+// var stdCourse='Certified Mobile application development';;
+//     alert(stdName);
+//     alert(stdAge);
+//     alert(stdCourse);
+
+//5
+// var a='PIZZA';
+// var strLength=a.length;
+// for (i=0;i<strLength;i++)
+// alert(a+'\n'+a.slice(0,4)+'\n'+a.slice(0,3)+'\n'+a.slice(0,2)+'\n'+a.slice(0,1))
+
+//6
+// var email='murad@gmail.com';
+// alert('my email address is '+email);
+
+//7
+// var book='a smarter way to learn javascript';
+// alert(book);
+
+//8
+// var elRef=document.getElementById('content-disply-byjavascript');
+// elRef.innerHTML='Yah! I can write html content through javascript';
+
+//9
+// var str='▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬';
+// alert(str);
+
+
+
+
+// Assignment #3
+//----VARIABLES FOR NUMBERS -------
+
+//1
+// var age=15;
+// alert('I am '+age+ ' years old');
+
+//2
+
+// function checkCookie() {
+//     var username = getCookie("username");
+//     if (username != "") {
+//      alert("Welcome again " + username);
+//     } else {
+//       username = prompt("Please enter your name:", "");
+//       if (username != "" && username != null) {
+//         setCookie("username", username, 365);
+//       }
+//     }
+//   }
+
+//   console.log(checkCookie());
+
+
+//3
+// var birthYear='1995';
+// var elRef=document.getElementById('my-birth-year');
+//     elRef.innerHTML='My birth year is '+birthYear+ '<br/> data type of my declared variable is number'
+
+
+//4
+// var VisitorName = prompt('Please enter your name');
+// var firstLetter = VisitorName.slice(0, 1).toLocaleUpperCase();
+// var otherLetters = VisitorName.slice(1);
+// VisitorName = firstLetter + otherLetters;
+// var EnterQuantity = prompt('Please enter quantity of product');
+// var productTitle = prompt('Please enter product Title you want to buy');
+// var visitorData = {
+//     name: VisitorName,
+//     quantity: EnterQuantity,
+//     product: productTitle
+// }
+
+// var elRef = document.getElementById('visitor-data');
+// elRef.innerHTML = visitorData.name + ' ordered ' + visitorData.quantity + ' of ' +visitorData.product;
+
+
+
