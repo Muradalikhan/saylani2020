567b976f9f5905265b43a423d9ae90ac19599484
unknown <murad.jaddoon@gmail.com>
2020-07-22 14:06:22 +0500

    58 to 67

---
+++ Assignment of Js/chap 58 to 67/app.js
@@ -0,0 +1,40 @@
+
+
+// task 1
+// var getRef=document.getElementById('main-content');
+// var getFirstName=document.getElementById('first-name');
+// var getLastName=document.getElementById('last-name');
+
+
+// console.log(getRef.childNodes)
+
+// var getClassRender=document.getElementsByClassName('render')
+// document.write('<h1>Render Element</h1>')
+// for(var i=0;i<getClassRender.length;i++)
+// document.write(getClassRender[i].innerHTML)
+
+// getFirstName.value='Murad';
+// getLastName.value='Khan';
+
+
+
+// task 2
+var getFormContent=document.getElementById('form-content');
+var getLastName=document.getElementById('lastName');
+var getRef=document.getElementById('main-content');
+var email=document.getElementById('email');
+
+
+// console.log(getFormContent.nodeType)
+// console.log(getLastName.nodeType)
+// console.log(getLastName.childNodes)
+// var c=getLastName.childNodes[0];
+// console.log(c)
+// getLastName.innerHTML=' i am updated'
+
+// console.log(getRef.firstChild)
+// console.log(getRef.lastChild)
+// console.log(getLastName.previousSibling)
+// console.log(getLastName.nextSibling)
+// console.log(email.parentNode)
+// console.log(email.nodeType)
